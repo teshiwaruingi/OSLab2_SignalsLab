4f91988758723addef0fd786a1b39e134efa9de2
Teshi Waruingi <71100831+teshiwaruingi@users.noreply.github.com>
2022-12-13 18:12:32 -0500

    Update signal.c

--- signal.c
+++ signal.c
@@ -4,23 +4,32 @@
 #include <signal.h>
 #include <unistd.h>
 
+// a global variable to track whether the signal has been received
 int flag = 0;
+
+// the signal handler function
 void handler(int signum)
-{ //signal handler
+{
   printf("Hello World!\n");
   flag = 1;
-//   exit(1); //exit after printing
 }
 
 int main(int argc, char * argv[])
 {
-  signal(SIGALRM,handler); //register handler to handle SIGALRM
+  // register the signal handler
+  signal(SIGALRM, handler);
+
+  // busy wait until the signal is received
   while (flag == 0) {
-     alarm(1); 
-     while(flag == 0) {
-  }; //busy wait for signal to be delivered
+    alarm(1);
+    while (flag == 0) {
+      // do nothing
+    }
+
+    // print a message and reset the flag
     printf("Turing was right!\n");
     flag = 0;
   }
-  return 0; //never reached
+
+  return 0;
 }
