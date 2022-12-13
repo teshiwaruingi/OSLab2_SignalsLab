0cd5bc61dd4964ccf5ced23996f1e55ca9de7695
Teshi Waruingi <71100831+teshiwaruingi@users.noreply.github.com>
2022-12-13 18:13:00 -0500

    Update timer.c

--- timer.c
+++ timer.c
@@ -5,31 +5,50 @@
 #include <time.h>
 #include <unistd.h>
 
+// a global variable to track whether the signal has been received
 int flag = 0;
-time_t seconds;
+
+// a global variable to track the number of alarms that have been received
 int alarms = 0;
 
+// a global variable to track the start time of the program
+time_t seconds;
+
+// the signal handler for the alarm
 void alarmSignalHandler(int signum)
-{ //signal handler
+{
   flag = 1;
 }
 
-void exitSignalHandler(int sigint) {
-  printf("Your Program excecuted %d alarms in %ld seconds \n",alarms, (time(NULL)-seconds));
+// the signal handler for the exit signal
+void exitSignalHandler(int sigint)
+{
+  printf("Your Program excecuted %d alarms in %ld seconds \n", alarms, (time(NULL) - seconds));
   exit(1);
 }
 
-int main(int argc, char * argv[]) {
+int main(int argc, char * argv[])
+{
+  // store the start time of the program
   seconds = time(NULL);
+
   printf("Your program is running...\n");
+
+  // register the signal handlers
   signal(SIGALRM, alarmSignalHandler);
   signal(SIGINT, exitSignalHandler);
+
+  // busy wait until the signal is received
   while (1) {
-     alarm(1);
-     while(flag == 0){}; 
-     alarms += 1;
-     flag = 0;
+    alarm(1);
+    while (flag == 0) {
+      // do nothing
+    }
+
+    // increment the alarm counter and reset the flag
+    alarms += 1;
+    flag = 0;
   }
+
   return 0;
 }
-
